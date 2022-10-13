bc666d3f0c28719616d9ff29d8c3101057b7a0b8
Pavel_Vatsek <pvatsek@gmail.com>
2022-10-13 10:21:03 +0300

    dd Example 004 Div

--- example004_DIV/Program.cs
+++ example004_DIV/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿//Деление в целых числах
+//int numberA = 12;
+//int numberB = 5;
+//Console.WriteLine(numberA / numberB);
+
+//Деление с вещественными числами (с десятыми, сотыми и т.д.)
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
