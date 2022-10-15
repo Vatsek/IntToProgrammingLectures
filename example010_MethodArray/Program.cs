c2f78ae5536cfc25d2ffe825f5b65e09a620df65
Pavel_Vatsek <pvatsek@gmail.com>
2022-10-15 14:05:37 +0300

    add lecture 2

---
+++ example010_MethodArray/Program.cs
@@ -0,0 +1,20 @@
+﻿// Поиск индекса числа по содержимому
+//             0  1  2  3  4   5   6   7
+int[] array = {1, 5, 8, 4, 35, 46, 95, 35, 34};
+
+int n = array.Length; // в переменной n будет назначена длина массива
+ 
+ int find = 35;
+
+ int index = 0;
+
+ while (index < n)
+ {
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index +1 ниже то же самое
+    index++;
+ }
---
+++ example011_ArrayLibrary/Program.cs
@@ -0,0 +1,53 @@
+﻿void FillArray(int[] collection) // void метод заполнения массива случайными числами от 1 до 10
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col) // void метод который выводит в консоль все элементы массива
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+        
+    }
+    return position;
+}
+
+
+
+
+int[] array = new int[10]; /* new int[10]; - создай новый массив в котором 10 элементов. 
+По умочанию он заполнен нулямию  */
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 45);
+Console.WriteLine(pos);
